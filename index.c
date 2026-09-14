322645213208963ccf4ebd1f2856bc4d55987633
Tiago Gomes Luiz <tiago.gomes.luiz@hotmail.com>
2026-09-14 00:54:05 -0300

    primeiros passos do prototipo em C no index

--- index.c
+++ index.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//O Struct é o formato do produto que será apresentado ao sistema
+struct Produto{
+    int codigo;
+    char [50];
+    float preco;
+};
 
 int main(){
+    int opcao;
 
-    int idade;
+    
     
 
 
