205818a7430f73b947841ba85b7713d9232768c7
Tiago Gomes Luiz <tiago.gomes.luiz@hotmail.com>
2026-09-16 00:38:45 -0300

    criacao da primeira tela de opcoes

--- index.c
+++ index.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//O Struct é o formato do produto que será apresentado ao sistema
-struct Produto{
-    int codigo;
-    char [50];
-    float preco;
-};
-
 int main(){
-    int opcao;
-
     
-    
+    do{
+        printf("\n---OPCOES DE ACESSO---\n");
+        printf("1- ADMINISTRADOR\n");
+        printf("2- FAZER PEDIDO\n");
+        printf("3- CAIXA\n");
+        printf("4- SAIR\n");
+        printf("\n\nESCOLHA UMA OPÇÃO: ");
+        scanf("%i", &opcao);
 
+        switch(opcao) {
+            case 1:
+                printf("\n-> ACESSO DE ADMINISTRADOR\n");
+                break;
+            case 2:
+                printf("\n-> FAÇA SEU PEDIDO\n");
+                break;
+            case 3:
+                printf("\n-> ACESSO CAIXA\n");
+                break;
+            case 4:
+                printf("\n-> VOCE ESTA SAINDO\n");
+                break;
+            
+            default()
+        }
+
+    }while(opcao != 0);
 
     return 0;
 }
